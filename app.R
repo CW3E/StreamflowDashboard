2477ac84219b8e94051c93e69dd73b77be817291
Anahita Jensen <anahita.onyx@gmail.com>
2023-07-21 13:29:56 -0700

    Error with plotting x axis(date) on hydrograph

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-#load packages----------------------------------------------------------------------------------------------------------------------------
+#load packages ---------------------------------------------------------------------------------------------------------------------------
 
 library(shiny)
 library(ggplot2)
@@ -10,7 +10,6 @@ library(shinythemes)
 library(leaflet)
 library(tidyverse)
 library(DT)
-library(data.table)
 
 #data loading and formatting--------------------------------------------------------------------------------------------------------------
 
@@ -20,60 +19,77 @@ setwd("C:/Users/anahi/OneDrive/Documents")
 stat_location <- read.csv("GitHub/R-shiny/Data/Stream/CW3E_Stations_all.csv")
 stat_location$Site.Type <- gsub("SMOIL", "Smoil", stat_location$Site.Type)
 
-#github data
-BYS_obs <- read.csv("GitHub/R-shiny/Data/Stream/BYS_stream_obs.csv")
-#manuall <- BYS_obs$Q.cfs
-#CLD_obs <- read.csv("GitHub/R-shiny/Data/Stream/CLD_stream_obs.csv")
-
-#precipitation data
+#precipitation data from github
 precip <- read_excel("GitHub/R-shiny/Data/Stream/BCC_all_precip.xlsx")
-# Aggregate the data to 15-minute intervals
-precip15 <- precip %>%
-  group_by(TIMESTAMP = cut(TIMESTAMP, "15 mins")) %>%
-  summarise(Rain_mm_Tot = sum(Rain_mm_Tot))
+#Aggregate the data to 15-minute intervals
+precip15 <- precip %>%group_by(TIMESTAMP=cut(TIMESTAMP,"15 mins"))%>%summarise(Rain_mm_Tot=sum(Rain_mm_Tot))
 
-#need to figure out how to clean up this data
+#precipitation data from skyriver
 #BCC <- read.csv("//Skyriver/CW3E_data/CW3E_SurfaceMet_Archive/BCC/BoyesCreekCanyon_TwoMinWS.dat")
 #BVS <- read.csv("//Skyriver/CW3E_data/CW3E_SurfaceMet_Archive/BVS/BrownsValleySchool_TwoMin.dat")
 #DRW <- read.table("//Skyriver/CW3E_data/CW3E_SurfaceMet_Archive/DRW/Deerwood_TwoMinWS.dat")
 #WDG <- read.table("//Skyriver/CW3E_data/CW3E_SurfaceMet_Archive/WDG/WindyGap_TwoMinWS.dat",fill=TRUE)
 
-rainfall = precip15$Rain_mm_Tot 
-
-rainAx = list(overlaying = "y",side = "right",title = "Precipitation (mm)",range = c(300,0),showgrid=FALSE)
-
 #streamflow data
-BYS <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/BYS/Processed/BYS_LogLog_Q.csv")
-CLD <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/CLD/Processed/CLD_LogLog_Q.csv")
-MEW <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/MEW/Processed/MEW_LogLog_Q.csv")
-MLL <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/MLL/Processed/MLL_LogLog_Q.csv")
-PRY <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/PRY/Processed/PRY_LogLog_Q.csv")
-WHT <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/WHT/Processed/WHT_LogLog_Q.csv")
-#UDC <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/WHT/Processed/UDC_LogLog_Q.csv")
-#SYR <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/WHT/Processed/SYR_LogLog_Q.csv")
-#LDM <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/WHT/Processed/LDm_LogLog_Q.csv")
-#SAP <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/SAP/Processed")
+BYS_Q <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/BYS/Processed/BYS_LogLog_Q.csv")
+CLD_Q <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/CLD/Processed/CLD_LogLog_Q.csv")
+MEW_Q <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/MEW/Processed/MEW_LogLog_Q.csv")
+MLL_Q <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/MLL/Processed/MLL_LogLog_Q.csv")
+PRY_Q <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/PRY/Processed/PRY_LogLog_Q.csv")
+WHT_Q <- read.csv("//Skyriver/CW3E_data/CW3E_Streamflow_Archive/WHT/Processed/WHT_LogLog_Q.csv")
 
-#format the dates from flow 
-BYS$bys.dt2= as.POSIXct(BYS$bys.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
-CLD$cld.dt2= as.POSIXct(CLD$cld.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
-MEW$mew.dt2= as.POSIXct(MEW$mew.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
-MLL$mill.dt2= as.POSIXct(MLL$mill.dt2,tz= "UTC",format= "%Y-%m-%d %H:%M:%S")
-PRY$pry.dt2= as.POSIXct(PRY$pry.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
-WHT$wht.dt2= as.POSIXct(WHT$wht.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
-
-#streamflow data    #this won't work as long as files are in separate folders, too slow
-#flow_data = list.files(path = "//Skyriver/CW3E_data/CW3E_Streamflow_Archive/",pattern="*_LogLog_Q.csv",recursive=TRUE)
-#flow = lapply(flow_data, read.csv)
-
-setwd("Z:/")
+#format streamflow data timestamps
+BYS_Q$bys.dt2= as.POSIXct(BYS_Q$bys.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
+CLD_Q$cld.dt2= as.POSIXct(CLD_Q$cld.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
+MEW_Q$mew.dt2= as.POSIXct(MEW_Q$mew.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
+MLL_Q$mill.dt2=as.POSIXct(MLL_Q$mill.dt2,tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
+PRY_Q$pry.dt2= as.POSIXct(PRY_Q$pry.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
+WHT_Q$wht.dt2= as.POSIXct(WHT_Q$wht.dt2, tz= "UTC", format= "%Y-%m-%d %H:%M:%S")
 
 #manual streamflow data
-manual_data = list.files(path = "CW3E_Streamflow_Archive/Data/",pattern="*Manual_Q_R.xlsx")
-#manual = lapply(manual_data,read.csv)
+BYS_QM <- read_xlsx("Z:/CW3E_Streamflow_Archive/Data/BYS_Manual_Q_R.xlsx")
+CLD_QM <- read_xlsx("Z:/CW3E_Streamflow_Archive/Data/CLD_Manual_Q_R.xlsx")
+MEW_QM <- read_xlsx("Z:/CW3E_Streamflow_Archive/Data/MEW_Manual_Q_R.xlsx")
+MLL_QM <- read_xlsx("Z:/CW3E_Streamflow_Archive/Data/MLL_Manual_Q_R.xlsx")
+PRY_QM <- read_xlsx("Z:/CW3E_Streamflow_Archive/Data/PRY_Manual_Q_R.xlsx")
+WHT_QM <- read_xlsx("Z:/CW3E_Streamflow_Archive/Data/WHT_Manual_Q_R.xlsx")
 
-#stage data 
-stage_data = list.files(path = "CW3E_Streamflow_Archive/Data/All_Barocorrected_Level/",pattern="*.csv")
+#merge precipitation with streamflow (using CLD_QM as test for manual data)
+merged <- merge(BYS_Q,precip15,by.x ="bys.dt2", by.y = "TIMESTAMP",all = TRUE)
+merged <- merge(CLD_Q,merged, by.x = "cld.dt2", by.y = "bys.dt2", all = TRUE)
+merged <- merge(MEW_Q,merged, by.x = "mew.dt2", by.y = "cld.dt2", all = TRUE)
+merged <- merge(MLL_Q,merged, by.x = "mill.dt2",by.y = "mew.dt2", all = TRUE)
+merged <- merge(PRY_Q,merged, by.x = "pry.dt2", by.y = "mill.dt2",all = TRUE)
+merged <- merge(WHT_Q,merged, by.x = "wht.dt2", by.y = "pry.dt2", all = TRUE)
+merged <- merge(CLD_QM,merged, by.x = "Time.UTC", by.y = "wht.dt2", all=TRUE)
+
+#attempts to merge all streamflow data at once,error: 'join columns in x must be present in data'
+#merge_Q <- list(BYS_Q,CLD_Q,MEW_Q,MLL_Q,PRY_Q,WHT_Q)
+#merged_Q <- merge_Q %>% reduce(full_join, by=join_by("*dt2"))
+
+CLD_M = merged$Q.cfs
+BYS = merged$bys.q4 
+CLD = merged$cld.q3 
+MEW = merged$mew.q3
+MLL = merged$mill.q3
+PRY = merged$pry.q3
+WHT = merged$wht.q3
+date = merged$wht.dt2
+rain = merged$Rain_mm_Tot 
+
+#set right y axis for precipitation
+rainAx = list(overlaying = "y",side = "right",title = "Precipitation (mm)",range = c(300,0),showgrid=FALSE)
+
+#set wd to //Skyriver/CW3E_data/
+setwd("Z:/")
+
+#manual streamflow data, this used to work and then stopped working and i dont know why :(
+#was trying this way to see if its faster but it doenst work, issue with the lapply function and reading path of file
+#manual_data = list.files(path = "CW3E_Streamflow_Archive/Data/",pattern="*Manual_Q_R.xlsx")
+#manual = lapply(manual_data,read_xlsx)
+
+#stage data, same issue as above
+#stage_data = list.files(path = "CW3E_Streamflow_Archive/Data/All_Barocorrected_Level/",pattern="*.csv")
 #stage = lapply(stage_data,read.csv)
 
 #stage[[1]]$TIMESTAMP.UTC=as.POSIXct(stage[[1]]$TIMESTAMP.UTC,tz="UTC",format= "%Y-%m-%d %H:%M:%S")
@@ -82,9 +98,6 @@ stage_data = list.files(path = "CW3E_Streamflow_Archive/Data/All_Barocorrected_L
 #stage[[4]]$TIMESTAMP.UTC=as.POSIXct(stage[[4]]$TIMESTAMP.UTC,tz="UTC",format= "%Y-%m-%d %H:%M:%S")
 #stage[[5]]$TIMESTAMP.UTC=as.POSIXct(stage[[5]]$TIMESTAMP.UTC,tz="UTC",format= "%Y-%m-%d %H:%M:%S")
 #stage[[6]]$TIMESTAMP.UTC=as.POSIXct(stage[[6]]$TIMESTAMP.UTC,tz="UTC",format= "%Y-%m-%d %H:%M:%S")
-
-#date data
-#date = BYS$bys.dt2
 
 #user interface--------------------------------------------------------------------------------------------------------------
 
@@ -109,30 +122,22 @@ ui <- fluidPage(
                                       selectizeInput(
                                         inputId = "select_station",
                                         label = "Select Station:",
-                                        choices = c("BYS","CLD","MEW","MLL","PRY","WHT","LDM","SYR","UDC"),
+                                        choices = stat_location$CW3E.Code,
                                         selected = "BYS"), br(),
                                       
                                       checkboxGroupInput(
-                                        inputId = "select_variable",
+                                        inputId = "var",
                                         label = "Select Variable(s):",
                                         choices = list("Streamflow", "Level"),           #precip is fixed, so did not include 
                                         selected="Streamflow"), br(),
                                       
-                                      #sliderInput(
-                                      #  inputId = "date_range",
-                                      # label = "Select Date Range:",
-                                      #  min = as.Date("2017-09-01"),
-                                      # max = as.Date("2022-01-01"),
-                                      #value = c(as.Date("2017-09-01"), as.Date("2022-01-01")),
-                                      #timeFormat="%Y-%m-%d"), 
-                                      
                                       sliderInput(
                                         inputId = "date_range",
                                         label = "Select Date Range:",
-                                        min = as.POSIXct("2017-09-09 12:30:00"),
-                                        max = as.POSIXct("2022-10-25 07:00:00"),
-                                        value=c(as.POSIXct("2017-09-09 12:30:00"),
-                                                as.POSIXct("2022-10-25 07:00:00")),
+                                        min = as.POSIXct("2015-09-09 12:30:00"),
+                                        max = as.POSIXct("2023-10-25 07:00:00"),
+                                        value=c(as.POSIXct("2015-09-09 12:30:00"),
+                                                as.POSIXct("2023-10-25 07:00:00")),
                                         timeFormat="%Y-%m-%d %H:%M:%S"),
                                       
                                       br(),br(),br(),br(),
@@ -179,45 +184,48 @@ server <- function(input,output,session){
   #hydrograph--------------------------------------------------------------------------------------------------------------------  
   
   y <- reactive({input$select_station})
-  #z <- reactive({input$select_variable})
-
+ 
   output$graph <- renderPlotly({
     
+    #this is for date slider, doesn't really work but also doesn't create errors so just leaving it for now
     filteredData <- subset(date, date >= input$date_range[1] & date <= input$date_range[2])
-    
+
     plot_ly() %>%
+      
+      #adding points for manual streamflow data, also attempted with add_trace below
+      add_markers(
+                  x=~date,
+                  y=~CLD_M,
+                  type="scatter",
+                  marker = list
+                  (color ="blue"),name="Manual Streamflow") %>%
+      
+      #add_trace(
+      #           x=~date,
+       #          y=~CLD_M,
+        #         type="scatter",mode="markers",
+         #        name="Manual Streamflow")%>%
+      
+      #adding lines for streamflow data
       add_trace(filteredData,
                 x=~date,
                 y=~get(input$select_station),                  
                 type="scatter", mode="lines", line = list               
-                (color = '#2fa839', width = 1, 
-                  dash = 'solid'),name = "Streamflow") %>%   
-      
-      #Manual streamflow data
-      add_trace(
-        x=~date,
-        y=~manuall, type = "scatter", mode="markers",
-        color='red',
-        size=10,
-        name = "Streamflow - Manual")  
-      
-      add_trace(filteredData,
-                #choose precipitation station based on chosen stream station, nested ifelse statement
-                #ifelse(~get(input$select_station)=="BYS",y="ATP",
-                #      ifelse(~get(input$select_station)=="CLD"|y=="PRY"|y=="WHT"|y=="EFR",y="CPP",
-                #            ifelse(~get(input$select_station)=="MEW",y="DPR",y="RVP"))),
-                
-                x=~date,
-                y=~rainfall,
-                type="bar", yaxis="y2", marker = list
-                (color ="blue",width = 1),name = 'Precipitation - BCC') %>%  
-      
-      layout(#title = "Rainfall-Streamflow",
-        xaxis =list
-        (title = "time (daily)"), yaxis=list
-        (title="Q  ft³/s",range=c(0,60)),yaxis2=rainAx)
-  })
+                (color='#2fa839',width=1,dash='solid'),name="Streamflow") %>%      
 
+      #precipitation
+      add_trace(filteredData,
+                x=~date,
+                y=~rain,
+                type="bar", yaxis="y2", marker = list
+                (color="blue",width=1),name='Precipitation') %>%  
+      
+      layout(
+        xaxis =list
+        (title = "Time (daily)"), yaxis=list
+        (title="Q (ft³/s)",range=c(0,60)),yaxis2=rainAx)
+  })
+  
   #map of stations-------------------------------------------------------------------------------------------------------------
   
   RdYlBu <- colorFactor("RdYlBu",domain=stat_location$Site.Type)
@@ -248,12 +256,12 @@ server <- function(input,output,session){
   
   #download data------------------------------------------------------------------------------------------------------------------
   
-  #data <- reactive({get(input$dataset)})
+  data <- reactive({get(input$dataset)})
   
-  #output$download_data <- downloadHandler(
-  #  filename = function(){paste("GitHub/R-shiny/Data/Stream/",input$dataset,"_LogLog_Q_GM.csv")},
-  #  content=function(file){write.csv(data(),file)}
-  #)
+  output$download_data <- downloadHandler(
+    filename = function(){paste("GitHub/R-shiny/Data/Stream/",input$dataset,"_LogLog_Q_GM.csv")},
+    content=function(file){write.csv(data(),file)}
+  )
   
   #image for selected station----------------------------------------------------------------------------------------------------------------
   
